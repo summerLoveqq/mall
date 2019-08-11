6e4b91f9a5e9cc50ef420dd1a2a0735a49654036
xgb <18200112782@163.com>
2019-08-11 22:43:06 +0800

    add postcss config and css loader

---
+++ src/components/common/navBar/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+  display: flex;
+  height: 80px;
+  line-height: 80px;
+  .left {
+    width: 120px;
+    background: red;
+  }
+  .right {
+    width: 120px;
+    background: red;
+  }
+  .center {
+    flex: 1;
+    font-size: 40px;
+  }
+}
+</style>
---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="detail">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Detail",
+  }
+</script>
+
+<style scoped>
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
+
+  .detail-nav {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
+
+  .content {
+    height: calc(100% - 44px);
+  }
+</style>
